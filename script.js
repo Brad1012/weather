9dfe98e257e197eb92b051c908ce26275ac003a5
Bradley Beard <Brad1012@hotmail.co.uk>
2022-05-20 17:01:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let weather = {
     apikey : "20728caeb33fee324492cdc0e3acff87",
-    fetchWeather: function () {
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=20728caeb33fee324492cdc0e3acff87"
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city
+             + "&units=metric&appid="
+             + this.apikey
     )
     .then((response) => response.json())
-    .then((data) => console.log(data));
- },
+    .then((data) => this.displayWeather(data));
+        },
+        displayWeather: function(data) {
+            const { name } = data;
+            const { icon, description } = data.weather[0];
+            const { temp,humidity } = data.main;
+            const { speed } = data.wind;
+            console.log(name,icon,description,temp,humidity,speed)
+            document.querySelector(".city").innerText = "Weather in " + name;
+ }
 };
