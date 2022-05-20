5d3fc2e8ce4b0422bad3326853f9a32f7730a149
Bradley Beard <Brad1012@hotmail.co.uk>
2022-05-20 13:21:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let weather = {
+    "apikey" : "20728caeb33fee324492cdc0e3acff87",
+    fetchWeather: funcion () {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=20728caeb33fee324492cdc0e3acff87",
+    );
+ }
+}
