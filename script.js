8abe7025e6bfeb1ff06b56baf506f6be87601e09
Bradley Beard <Brad1012@hotmail.co.uk>
2022-05-23 10:47:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,11 @@ let weather = {
             const { speed } = data.wind;
             console.log(name,icon,description,temp,humidity,speed)
             document.querySelector(".city").innerText = "Weather in " + name;
+            document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+            document.querySelector(".description").innerText = "" + description;
+            document.querySelector(".temp").innerText = temp + "°";
+            document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
+            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
  }
 };
+
