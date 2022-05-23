75e25e54c6268c62c624bad18e53590cfcac1073
Bradley Beard <Brad1012@hotmail.co.uk>
2022-05-23 11:50:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,32 @@ let weather = {
             document.querySelector(".city").innerText = "Weather in " + name;
             document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
             document.querySelector(".description").innerText = "" + description;
-            document.querySelector(".temp").innerText = temp + "°";
-            document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
-            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+            document.querySelector(".temp").innerText = temp + "°C";
+            document.querySelector(".wind").innerText = 
+            "Wind Speed: " + speed + "Km/h";
+            document.querySelector(".humidity").innerText = 
+            "Humidity: " + humidity + "%";
+            document.querySelector(".weather").classList.remove("loading");
+          document.body.style.backgoundImage = "url('https://source.unsplash.com2560x1440/?," + name +
+            "')"
+ },
+ search: function() {
+     this.fetchWeather(document.querySelector(".search-bar").value);
  }
 };
 
+document
+.querySelector(".search button")
+.addEventListener("click", function() {
+weather.search();
+});
+
+document.querySelector(".search-bar")
+.addEventListener("keyup", function (event) {
+if (event.key == "Enter") {
+    weather.search();
+}
+});
+
+
+weather.fetchWeather("London");
